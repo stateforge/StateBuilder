0f4135b258eb1d6a8ad512ae302a8175135c3695
Frederic Heem <frederic.heem@gmail.com>
2015-03-31 21:48:37 +0200

    StateBuilderCpp 3.0.0

--- StateBuilderCpp/script/postInstall.bat
+++ StateBuilderCpp/script/postInstall.bat
@@ -1,3 +1,3 @@
-start http://www.stateforge.com/Deploy/Install/StateBuilderCpp?version=2.7.0
+start http://www.stateforge.com/Deploy/Install/StateBuilderCpp?version=3.0.0
 call explorer.exe %1
 echo done
--- StateBuilderCpp/script/preUninstall.bat
+++ StateBuilderCpp/script/preUninstall.bat
@@ -1,1 +1,1 @@
-start http://www.stateforge.com/Deploy/Uninstall/StateBuilderCpp?version=2.7.0
+start http://www.stateforge.com/Deploy/Uninstall/StateBuilderCpp?version=3.0.0
